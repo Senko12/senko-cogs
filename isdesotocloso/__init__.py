25dcd123a1c37fa519b999daf54bc08f3fde2dda
RedBanana <93483539+Senko12@users.noreply.github.com>
2025-02-18 22:29:44 -0600

    Update __init__.py

--- isdesotocloso/__init__.py
+++ isdesotocloso/__init__.py
@@ -1,4 +1,7 @@
+# __init__.py
+
 from .pymyweather import SchoolClosureCog
 
 def setup(bot):
+    # Remove 'await' from bot.add_cog() since it's not a coroutine
     bot.add_cog(SchoolClosureCog(bot))
